8f0b32159a5ca9bf222fab42c4dff55b493a2562
Muhammad Naeem Raj <90338269+muhammadnaeemraj@users.noreply.github.com>
2022-11-21 18:41:23 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Counter from "./component/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Counter App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Counter.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Counter = () => {
+  return (
+    <>
+      <h3>Press for increment red and for decrement black button</h3>
+      <button>Increment</button>
+      <button>Decrement</button>
+    </>
+  );
+};
+
+export default Counter;
