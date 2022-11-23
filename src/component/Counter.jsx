f74fa28f29e2ffceb5163c8dd8af3675658531b5
Muhammad Naeem Raj <90338269+muhammadnaeemraj@users.noreply.github.com>
2022-11-23 04:47:45 +0000

    updated the counter app

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useReducer } from "react";
+
+const initialState = 1;
+const reducer = (state, action) => {
+  if (action == "increment") {
+    return state + 1;
+  } else if (action == "decrement") {
+    return state - 1;
+  } else {
+    return state;
+  }
+};
 
 const Counter = () => {
+  const [count, dispatch] = useReducer(reducer, initialState);
   return (
     <>
       <h3>Press for increment red and for decrement black button</h3>
-      <button>Increment</button>
-      <button>Decrement</button>
+      <h2>{count}</h2>
+      <button onClick={() => dispatch("increment")}>Increment</button>
+      <button onClick={() => dispatch("decrement")}>Decrement</button>
     </>
   );
 };
