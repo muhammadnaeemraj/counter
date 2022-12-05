1d604bfea5c04e58c17fc4028b135f9fb7a55524
Muhammad Naeem Raj <90338269+muhammadnaeemraj@users.noreply.github.com>
2022-12-05 08:10:14 +0000

    2

--- src/component/Calculator.jsx
+++ src/component/Calculator.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useRef, useState } from "react";
 
 function Calculator() {
-  const inputRef = useRef(null);
+  let inputRef = useRef(null);
   const resultRef = useRef(null);
   const [result, setResult] = useState(0);
 
@@ -22,23 +22,34 @@ function Calculator() {
     e.preventDefault();
     setResult((res) => res / Number(inputRef.current.value));
   };
+  const resetInput = (e) => {
+    e.preventDefault();
+    inputRef.current.value = null;
+  };
+  const resetResult = (e) => {
+    e.preventDefault();
+    setResult(()=> 0 * Number(inputRef.current.value));
+  };
   return (
     <>
       <div className="cal">
         <h1>Simple Calculator</h1>
         <form>
           <p ref={resultRef}>{result}</p>
-          <br />
+          {/* <br /> */}
           <input type="number" placeholder="Type a Number" ref={inputRef} />
-          <br />
-          <br />
+          <br /><br />
           <button onClick={Add}>Addition</button>
           <button onClick={Sub}>Subtract</button>
           <button onClick={Mul}>Multiplication</button>
           <button onClick={Div}>Division</button>
+          <br />
+          <button onClick={resetInput}>Reset Input</button>
+          <button onClick={resetResult}>Reset Result</button>
         </form>
       </div>
     </>
   );
 }
 export default Calculator;
+
